a7c3340cb76d79faee858ec79450debfd673ddc3
PRASANNA PANDIYAN <sriprasanna.p@gmail.com>
2018-10-17 16:03:58 +0000

    updated test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 version=$1 
+
+sleep 30;
+
 cont=$(sudo docker run -d -p 80:80 sriprasanna25/sapient-test:$version)
+
+sleep 5;
 
 output=$(curl -sL -w "%{http_code}\\n" "localhost" -o /dev/null)
 
